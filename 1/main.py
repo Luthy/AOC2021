fc1e437bf91632c6c9498403757d95988184cbba
Luthy <luthien.gr@gmail.com>
2021-12-28 13:41:06 +0100

    Added solution to first puzzle

---
+++ 1/main.py
@@ -0,0 +1,36 @@
+from typing import List
+import numpy as np 
+import pandas as pd
+
+totalMoves = 0
+inputArray = []
+
+def readFile(fileName):
+        fileObj = open(fileName, "r") #opens the file in read mode
+        words = fileObj.read().splitlines() #puts the file into an array
+        fileObj.close()
+        return words
+
+for row in readFile('C:/Users/Alexandros/Documents/Code/AOC2021/1/input.txt'):
+    inputArray.append(int(row))
+
+
+def countAmountOfDepthIncreases(inputData):
+    rowCounter = 0
+    higherThanLastCounter = 0   
+    for row in inputData:
+        if rowCounter>0 and row > lastRow:
+            higherThanLastCounter+=1
+        lastRow = row
+        rowCounter+=1
+
+    print(higherThanLastCounter)
+
+rowCounter = 0
+threePeriodAverage = []
+for row in inputArray:
+    if rowCounter > 1:
+        threePeriodAverage.append((inputArray[rowCounter]+inputArray[rowCounter-1]+inputArray[rowCounter-2])/3)
+    rowCounter+=1
+
+countAmountOfDepthIncreases(threePeriodAverage)
